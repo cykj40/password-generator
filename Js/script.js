ce52036e01514eacd6abd9bb1f471148f96ce394
cykj40 <102045473+cykj40@users.noreply.github.com>
2022-07-09 13:34:28 -0400

    linking css and Java script adding javascript

---
+++ Js/script.js
@@ -0,0 +1,28 @@
+// Assignment code here
+function generatePassword(){
+    var length = window.prompt("choose password length between 8 and 128")
+    char = "abcdefghijklmnopqrstuvwkyz"
+    num = "1234567890"
+    passwordText = [""]
+     for(i=0;i < length; i++){
+        passwordText += char.charAt(Math.floor(Math.random() * char.length)) 
+       
+     } return passwordText
+    };
+    
+    
+    
+    // Get references to the #generate element
+    var generateBtn = document.querySelector("#generate");
+    
+    // Write password to the #password input
+    function writePassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+    
+      passwordText.value = password;
+    
+    }
+    
+    // Add event listener to generate button
+    generateBtn.addEventListener("click", writePassword);
